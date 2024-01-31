6a067458e72a89f01c09b87272581fa2bf260956
StrictLine <miklos.moricz@strictline.biz>
2024-01-31 19:43:15 +0100

    generate-link: add prompt if url not provided by argument + run task

--- src/generate-link.ts
+++ src/generate-link.ts
@@ -2,16 +2,27 @@
 
 import { URL } from 'url';
 import * as dotenv from 'dotenv';
+import promptSync from 'prompt-sync';
 
 dotenv.config();
 const secret = process.env.ACCESS_KEY;
+const prompt = promptSync({ sigint: true });
+
 const publicHostUrl = process.env.PUBLIC_HOST_URL?.replace(/\/$/, '');
 
-const link = process.argv[2];
+let link = process.argv[2];
 
 if (!link) {
-	console.error('Please provide a link as an argument.');
-	process.exit(1);
+	// get user input by prompt
+	const userInput = prompt('Share link: ');
+	console.log(userInput);
+
+	if (!userInput) {
+		console.error('Please provide a link as an argument!');
+		process.exit(1);
+	}
+	
+	link = userInput;
 }
 
 const cleanedupLink = link.trim().replaceAll('\\', '');
